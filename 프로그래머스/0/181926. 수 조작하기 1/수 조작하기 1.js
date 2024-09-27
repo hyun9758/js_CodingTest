e298ab7fd18aef0c5bd8753a569318057307b939
Hyun <82191626+hyun9758@users.noreply.github.com>
2024-09-27 19:18:25 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.59 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,16 @@
+function solution(n, control) {
+    for (let i=0; i <= control.length; i++) {
+        let command = control[i];
+        
+        if (command === 'w') {
+            n += 1;
+        } else if (command === 's') {
+            n -= 1;
+        } else if (command === 'd') {
+            n += 10;
+        } else if (command === 'a') {
+            n -= 10;
+        }
+    }
+    return n;
+}
