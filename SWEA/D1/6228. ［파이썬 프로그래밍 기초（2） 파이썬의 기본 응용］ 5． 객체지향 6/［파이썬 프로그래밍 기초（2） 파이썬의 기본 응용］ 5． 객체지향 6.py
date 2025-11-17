9032489de76f4601dbee31d3609859e91b7c71de
Hyun <82191626+hyun9758@users.noreply.github.com>
2025-11-17 23:55:47 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 5. 객체지향 6, Time: 66 ms, Memory: 51,712 KB -BaekjoonHub

---
+++ SWEA/D1/6228. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 6/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 6.py
@@ -0,0 +1,13 @@
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length * self.length 
+
+Squ = Square(3)
+print("정사각형의 면적: %d"%Squ.area())
